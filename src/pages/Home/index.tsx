0c8095fad4ed0384d4ee577cca3e6ea597d542a8
cuiyao <yao.cui@plaud.ai>
2025-11-07 11:46:57 +0800

    首页更新

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,259 @@
 import './index.less';
-import banner0 from '@/assets/images/home/banner-0.png';
-import banner1 from '@/assets/images/home/banner-1.png';
-import banner2 from '@/assets/images/home/banner-2.png';
-import banner3 from '@/assets/images/home/banner-3.png';
-import banner4 from '@/assets/images/home/banner-4.png';
-import banner5 from '@/assets/images/home/banner-5.png';
+// import banner0 from '@/assets/images/home/banner-0.png';
+// import banner1 from '@/assets/images/home/banner-1.png';
+// import banner2 from '@/assets/images/home/banner-2.png';
+// import banner3 from '@/assets/images/home/banner-3.png';
+// import banner4 from '@/assets/images/home/banner-4.png';
+// import banner5 from '@/assets/images/home/banner-5.png';
+
+import heroProduct from '@/assets/images/home/hero-product.png';
+
+// 获奖徽章图片
+import awards from '@/assets/images/home/awards.png';
+
+// 放大人类智能 - 场景图片
+import aiScene1 from '@/assets/images/home/aiScene1.png';
+import aiScene2 from '@/assets/images/home/aiScene2.png';
+import aiScene3 from '@/assets/images/home/aiScene3.png';
+import aiScene4 from '@/assets/images/home/aiScene4.png';
+
+// 各领域专业人士 - 场景图片
+import professionalExecutive from '@/assets/images/home/professional-executive.png';
+import professionalSales from '@/assets/images/home/professional-sales.png';
+import professionalMedical from '@/assets/images/home/professional-medical.png';
+import professionalLawyer from '@/assets/images/home/professional-lawyer.png';
+import professionalEducator from '@/assets/images/home/professional-educator.png';
+import professionalCreator from '@/assets/images/home/professional-creator.png';
+
+// Plaud 产品系列图片
+import productNotePro from '@/assets/images/home/product-note-pro.png';
+import productNote from '@/assets/images/home/product-note.png';
+import productNotePinS from '@/assets/images/home/product-notepin-s.png';
+
+
+// plaud intelligence™ 图片
+import plaudIntelligence1 from '@/assets/images/home/plaud-intelligence-1.png';
+import plaudIntelligence2 from '@/assets/images/home/plaud-intelligence-2.png';
+import plaudIntelligence3 from '@/assets/images/home/plaud-intelligence-3.png';
+import plaudIntelligence4 from '@/assets/images/home/plaud-intelligence-4.png';
+import plaudIntelligence5 from '@/assets/images/home/plaud-intelligence-5.png';
+
+
+// 关注我们 - 图片
+import logoPlaud from '@/assets/images/home/logoPlaud.png';
+import channel from '@/assets/images/home/channel.png';
+
+// 关注我们 - 二维码
+import serviceQrCode from '@/assets/images/home/service-account.png';
+import videoQrCode from '@/assets/images/home/video-account.png';
+
+import androidQrCode from '@/assets/images/home/android.png';
+import iosQrCode from '@/assets/images/home/ios.png';
 
 const Home = () => {
     return (
         <div className="home">
-            {/* 主体图片 */}
-            <img src={banner0} alt="PLAUD 产品介绍" className="main-img" />
-            <img src={banner1} alt="PLAUD 产品介绍" className="main-img" />
-            <img src={banner2} alt="PLAUD 产品介绍" className="main-img" />
-            <img src={banner3} alt="PLAUD 产品介绍" className="main-img" />
-            <img src={banner4} alt="PLAUD 产品介绍" className="main-img" />
-            <img src={banner5} alt="PLAUD 产品介绍" className="main-img" />
+            {/* 顶部横幅 */}
+            <div className="hero-banner">
+                <div className="hero-content">
+                    <div className="hero-left">
+                        <div className="hero-left-content">
+                            <h2>全球畅销的AI纪要产品</h2>
+
+                            <div className="stats">
+                                <div className="stat-item">
+                                    <div> <span className="stat-number">170+</span>
+                                        <span className="stat-label">国家/地区</span></div>
+                                    <span className="stat-desc">产品畅销全球</span>
+                                </div>
+                                <div className="stat-item">
+                                    <div><span className="stat-number">1000+</span></div>
+                                    <span className="stat-label">全球媒体推荐</span>
+                                </div>
+                            </div>
+
+                            <div className="awards">
+                                <img src={awards} alt="获奖徽章" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="hero-right">
+                        <img className="product-showcase" alt="产品图片" src={heroProduct} />
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero-banner-mobile">
+                <div className="hero-content">
+                    <h2>全球畅销的AI纪要产品</h2>
+                    <div className="hero-content-desc">
+                        <img className="product-showcase" alt="产品图片" src={heroProduct} />
+                        <div className="stats">
+                            <div className="stat-item">
+                                <div>
+                                    <span className="stat-number">170+</span>
+                                    <span className="stat-label">国家/地区</span></div>
+                                <span className="stat-desc">产品畅销全球</span>
+                            </div>
+                            <div className="stat-item">
+                                <div><span className="stat-number">1000+</span></div>
+                                <span className="stat-label">全球媒体推荐</span>
+                            </div>
+                        </div>
+                        <div className="awards">
+                            <img src={awards} alt="获奖徽章" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* 第二块：放大人类智能 */}
+            <div className="section-ai-intelligence">
+                <div className="section-container">
+                    <div className="text-block">
+                        <h2 >放大人类智能</h2>
+                        <p className="section-description">
+                            我们相信对话即智能。对话是智能的一种载体，它是思想的开端、决策制定和意义的分享之地，是对世界认知的投射。但过去这些智能从未被捕捉、被理解、更未能转化为正确的决策和实际行动。
+                        </p>
+                        <p className="section-description">
+                            Plaud以“放大人类智能”为使命，致力于构建下一代智能基础设施与交互界面，旨在帮助用户从所听、所说、所见、所想中捕捉、提取和运用智能，成为全球专业人士最值得信赖的AI工作伙伴，全面提升生产力并创造更多价值。
+                        </p>
+                    </div>
+                    <div className="image-grid-4">
+                        <div className="grid-item">
+                            <img src={aiScene1} alt="场景1" />
+                        </div>
+                        <div className="grid-item">
+                            <img src={aiScene2} alt="场景2" />
+                        </div>
+                        <div className="grid-item">
+                            <img src={aiScene3} alt="场景3" />
+                        </div>
+                        <div className="grid-item">
+                            <img src={aiScene4} alt="场景4" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* 第三块：各领域专业人士 */}
+            <div className="section-professionals">
+                <div className="section-container">
+                    <h2 className="section-title-center">各领域专业人士值得信赖的AI工作伙伴<br />提升生产力和价值创造</h2>
+                    <div className="image-grid-6">
+                        <div className="grid-item">
+                            <img src={professionalExecutive} alt="高管" />
+                            <span className="grid-label">高管</span>
+                        </div>
+                        <div className="grid-item">
+                            <img src={professionalSales} alt="销售人士" />
+                            <span className="grid-label">销售人士</span>
+                        </div>
+                        <div className="grid-item">
+                            <img src={professionalMedical} alt="医疗专业人士" />
+                            <span className="grid-label">医疗专业人士</span>
+                        </div>
+                        <div className="grid-item">
+                            <img src={professionalLawyer} alt="律师" />
+                            <span className="grid-label">律师</span>
+                        </div>
+                        <div className="grid-item">
+                            <img src={professionalEducator} alt="教育工作者" />
+                            <span className="grid-label">教育工作者</span>
+                        </div>
+                        <div className="grid-item">
+                            <img src={professionalCreator} alt="内容创作者" />
+                            <span className="grid-label">内容创作者</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* 第四块：Plaud 产品系列 */}
+            <div className="section-products">
+                <div className="section-container">
+                    <h2 className="section-title-center">Plaud 硬件产品系列<br />由Plaud Intelligence™ 驱动</h2>
+
+
+                    <div className="product-cards">
+                        <img src={productNotePro} alt="Plaud Note Pro" />
+                        <img src={productNote} alt="Plaud Note" />
+                        <img src={productNotePinS} alt="Plaud NotePin S" />
+                    </div>
+                </div>
+            </div>
+
+
+            {/* 第五块：Plaud Intelligence™ */}
+            <div className="section-plaud-intelligence">
+                <div className="section-container">
+                    <h2 className="section-title-center">Plaud Intelligence™</h2>
+                    <div className="product-cards">
+                        <img src={plaudIntelligence1} alt="多模态输入" />
+                        <img src={plaudIntelligence2} alt="精准转写" />
+                        <img src={plaudIntelligence3} alt="多维总结" />
+                        <img src={plaudIntelligence4} alt="ask plaud" />
+                        <img src={plaudIntelligence5} alt="自动工作流" />
+                    </div>
+                </div>
+            </div>
+
+
+            {/* 第六块：关注我们 */}
+            <div className="section-follow-us">
+                <div className="section-container">
+                    <div className="logo">
+                        <img src={logoPlaud} alt="Plaud" />
+                    </div>
+
+                    <div className="follow-title">
+                        <h3>FOLLOW US</h3>
+                        <h4>关注我们</h4>
+                    </div>
+
+
+
+                    <div className='social-wrapper'>
+                        <img src={channel} alt="channel" />
+                        <div className="qr-section">
+
+                            <div className="qr-group">
+                                <div className="qr-item">
+                                    <div className="qr-placeholder">
+                                        <img src={serviceQrCode} alt="Plaud 服务号" />
+                                    </div>
+                                    <p>Plaud 服务号</p>
+                                </div>
+                                <div className="qr-item">
+                                    <div className="qr-placeholder">
+                                        <img src={videoQrCode} alt="Plaud 视频号" />
+                                    </div>
+                                    <p>Plaud 视频号</p>
+                                </div>
+                            </div>
+
+
+                            <div className="qr-group">
+                                <div className="qr-item">
+                                    <div className="qr-placeholder">
+                                        <img src={androidQrCode} alt="Android 下载" />
+                                    </div>
+                                    <p>Android 下载</p>
+                                </div>
+                                <div className="qr-item">
+                                    <div className="qr-placeholder">
+                                        <img src={iosQrCode} alt="iOS 下载" />
+                                    </div>
+                                    <p>ios 下载</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
             {/* 底部横幅 */}
             <div className="footer-banner">
